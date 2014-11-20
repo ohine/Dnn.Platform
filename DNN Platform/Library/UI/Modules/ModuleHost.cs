7808f8b35b79a70b6f8a505bff51253b5ef26420
Oliver Hine <oliver.hine@gmail.com>
2014-11-20 01:12:04 -0500

    switch telerik ajax panel for update panel

--- DNN Platform/Library/UI/Modules/ModuleHost.cs
+++ DNN Platform/Library/UI/Modules/ModuleHost.cs
@@ -338,24 +338,6 @@ namespace DotNetNuke.UI.Modules
             _control.ViewStateMode = ViewStateMode.Enabled;
         }
 
-        private void LoadAjaxPanel()
-        {
-            //var loadingPanel = new RadAjaxLoadingPanel { ID = _control.ID + "_Prog", Skin = "Default" };
-
-            //Controls.Add(loadingPanel);
-
-            //var ajaxPanel = new RadAjaxPanel {
-            //    ID = _control.ID + "_UP",
-            //    LoadingPanelID = loadingPanel.ID,
-            //    RestoreOriginalRenderDelegate = false
-            //};
-            //InjectMessageControl(ajaxPanel); 
-            //ajaxPanel.Controls.Add(_control);
-
-            //Controls.Add(ajaxPanel);
-
-        }
-
         /// <summary>
         /// LoadUpdatePanel optionally loads an AJAX Update Panel
         /// </summary>
@@ -508,7 +490,7 @@ namespace DotNetNuke.UI.Modules
                 //if module is dynamically loaded and AJAX is installed and the control supports partial rendering (defined in ModuleControls table )
                 if (!_isCached && _moduleConfiguration.ModuleControl.SupportsPartialRendering && AJAX.IsInstalled())
                 {
-                    LoadAjaxPanel();
+                    LoadUpdatePanel();
                 }
                 else
                 {
